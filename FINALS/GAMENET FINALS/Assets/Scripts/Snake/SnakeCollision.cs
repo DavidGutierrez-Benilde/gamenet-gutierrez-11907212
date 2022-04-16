3920f60751adc82576c15646cf647f67890d539b
DavidGutierrez-Benilde <davidandrew.gutierrez@benilde.edu.ph>
2022-04-16 16:33:08 +0800

    FINALS - Snake Collision Added!

---
+++ FINALS/GAMENET FINALS/Assets/Scripts/Snake/SnakeCollision.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SnakeCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("BodyPart"))
+        {
+            SnakeMovement snake = other.gameObject.GetComponentInParent<SnakeMovement>();
+
+            for (int i = 0; i < snake.initialSize; i++)
+            {
+                if (snake.BodyParts[i].gameObject == other.gameObject)
+                {
+                    Debug.Log("Hit itself");
+                    return;
+                }
+            }
+
+            snake.GetComponent<PhotonView>().RPC("RemoveBodyPart", RpcTarget.AllBuffered);
+
+            Vector3 spawnLocation = SpawnLocations.instance.spawnPoints
+                                                   [Random.Range(0, SpawnLocations.instance.spawnPoints.Count)].transform.position;
+
+            while (spawnLocation == snake.transform.position)
+                spawnLocation = SpawnLocations.instance.spawnPoints
+                                                    [Random.Range(0, SpawnLocations.instance.spawnPoints.Count)].transform.position;
+
+            for (int i = 0; i < snake.BodyParts.Count; i++) snake.BodyParts[i].transform.position = spawnLocation;
+        }
+    }
+}
